44f45f32da44495d2b151da28cc396516f762502
philables <phillipables1@gmail.com>
2018-01-03 19:05:24 -0500

    IActivity array with test case id 1

---
+++ run-journal/src/app/shared/activities.ts
@@ -0,0 +1,12 @@
+import { IActivity } from './activity.model.ts';
+
+expost const SAVED_ACTIVITIES: IActivity[] = [
+{
+	"id" : 1,
+	"name" : "Main Bike Trails",
+	"date" : new Date(01/02/2017),
+	"distance" : 6.2,
+	"comments" : "Nice day, cool temps",
+	"gpxData" : '../../assets/gpx/1.gpx'
+}
+]
