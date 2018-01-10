57a01b9756520bd8b562af1ef663c11e0f97e812
philables <phillipables1@gmail.com>
2018-01-09 22:09:59 -0500

    import components into map service

--- run-journal/src/app/services/map.service.ts
+++ run-journal/src/app/services/map.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment';
+import { IActivity } from '../shared/activity.model';
+import { ActivityService } from '../services/activity.service';
+import { SAVED_ACTIVITIES } from '../shared/activities';
 
 @Injectables()
 export class MapService {
