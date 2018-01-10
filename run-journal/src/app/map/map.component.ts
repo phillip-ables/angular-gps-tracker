0321ec80034d2d79f2ecebad1966ddc483513ff7
philables <phillipables1@gmail.com>
2018-01-09 23:42:30 -0500

    ngAfterView life cycle hook after view is initialized

--- run-journal/src/app/map/map.component.ts
+++ run-journal/src/app/map/map.component.ts
@@ -25,4 +25,13 @@ export class MapComponent implements OnInit {
   		+this._route.snapshot.params['id'])
   }
 
+  ngAfterViewInit(){
+    this._mapService.plotActivity(+this._route_snapshot.params[id]);
+    this.activityName = this.activity.name;
+    this.activityComments = this.activity.comments;
+    this.activityDistance = this.activity.distance;
+    this.activityDate = this.activity.Date;
+    this.gpx = this.activity.gpxData;
+  }
+
 }
