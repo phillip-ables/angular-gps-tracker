5d5348f4534227a3f188bb4709316eecd3412009
philables <phillipables1@gmail.com>
2018-01-09 22:27:56 -0500

    function to create a copy of activity list and define lambda hahah idk what he said but it sets the run id

--- run-journal/src/app/services/map.service.ts
+++ run-journal/src/app/services/map.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { enviroment } from '../../enviroments/enviroment';
+import { environment } from '../../environments/environment';
 import { IActivity } from '../shared/activity.model';
 import { ActivityService } from '../services/activity.service';
 import { SAVED_ACTIVITIES } from '../shared/activities';
 
+var apiToken = environment.MAPBOX_API_KEY;
+declare var omnivore: any;
+declare var L: any;
+
 @Injectable()
 export class MapService {
+    
+    getActivity(id: number){
+    	return SAVED_ACTIVITIES.slice(0).find(run => run.id == id)
+    }
+
 
 	constructor() { }
 
