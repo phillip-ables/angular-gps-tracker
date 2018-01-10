cc95376c7ffd51a37aa6298b3aec7d399a1b7416
philables <phillipables1@gmail.com>
2018-01-09 22:15:57 -0500

    add directory enviroment for api key mapbox, blank apikey

--- run-journal/src/app/services/map.service.ts
+++ run-journal/src/app/services/map.service.ts
@@ -4,7 +4,7 @@ import { IActivity } from '../shared/activity.model';
 import { ActivityService } from '../services/activity.service';
 import { SAVED_ACTIVITIES } from '../shared/activities';
 
-@Injectables()
+@Injectable()
 export class MapService {
 
 	constructor() { }
